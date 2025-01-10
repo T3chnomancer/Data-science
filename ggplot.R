e1886cee6472f38b314904fc81514d4c73f6fb4d
Hrithik Purohit <59143569+T3chnomancer@users.noreply.github.com>
2025-01-10 14:24:37 +0530

    Add files via upload
    r exercies

---
+++ ggplot.R
@@ -0,0 +1,10 @@
+library(datasets)
+data(mtcars)
+head(mtcars, 5)
+?mtcars
+library(ggplot2)
+ggplot(aes(x=disp,y=mpg),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon")+labs(x= "Displacement", y= "Miles per gallon")
+mtcars$vs <- as.factor(mtcars$vs)
+ggplot(aes(x=vs, y=mpg),data=mtcars)+geom_boxplot(alpha=0.3)+theme(legend.position="none")
+ggplot(aes(x=wt),data=mtcars)+geom_histogram(binwidth = 0.5)
+print(mtcars$vs)
